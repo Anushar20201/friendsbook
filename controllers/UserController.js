39c95903aacd9b4a93492674aa4f9dd9a3e192e1
Anusha <anushar20201@gmail.com>
2022-08-27 11:53:10 -0500

    adding create, find and update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,69 @@
 const { User } = require('../models');
 
 const userController = {
-  // the functions will go in here as methods
+
+  // getting all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  //Getting single user by its _id and populated thought and friend data
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate([{
+        path: 'thoughts',
+        select: '-__v'
+      },
+      {
+        path: "friends",
+        select: "-__v",
+      },
+      ])
+      .select('-__v')
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'Sorry no user found' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  //Posting a New User
+  createNewUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+  //update a User by its Id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'Sorry no user found' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+
 
 };
 
